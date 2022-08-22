ad4edc3f95299ab2973736fa0667869810411abf
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-08-22 18:01:41 +0900

    리액트 변환중 2

---
+++ src/App.js 17-57-46-853.js
@@ -0,0 +1,18 @@
+/*eslint-disable*/
+
+import styled from 'styled-components';
+import Header from './Body';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <ListHeader />
+      <Body />
+    </div>
+      );
+    }
+
+export default App;
